88b91974b76394eb40a6b430bd47cac9a64ede43
Dmitry Grigorenko <DS.Grigorenko13@gmail.com>
2017-11-10 12:09:02 +0300

    Task 'SawII'

---
+++ saw2_budget.py
@@ -0,0 +1,6 @@
+import tmdb
+
+
+movie_info = tmdb.make_tmdb_api_request(method='/movie/215', api_key=tmdb.api)
+print(str(movie_info['budget'])+'$')
+input()
---
+++ tmdb.py
@@ -0,0 +1,23 @@
+import urllib.request
+import urllib.parse
+import json
+
+api = '556bd0fed5f14fdc783703b508cc32b1'
+
+def load_json_data_from_url(base_url, url_params):
+    url = '%s?%s' % (base_url, urllib.parse.urlencode(url_params))
+    response = urllib.request.urlopen(url).read().decode('utf-8')
+    return json.loads(response)
+
+
+def make_tmdb_api_request(method, api_key, extra_params=None):
+    extra_params = extra_params or {}
+    url = 'https://api.themoviedb.org/3%s' % method
+    params = {
+        'api_key': api_key,
+        'language': 'ru',
+    }
+    params.update(extra_params)
+    return load_json_data_from_url(url, params)
+
+
