bc708b9681786f8e9fb051ab6404f5a2493f463e
Dmitry Grigorenko <DS.Grigorenko13@gmail.com>
2017-11-12 00:50:30 +0300

    Making collection update

--- make_collection.py
+++ make_collection.py
@@ -1,20 +1,25 @@
 import tmdb
 import json
+
 from pprint import pprint
 import time
+
 from sys import stdout
 
 print('Введите год:')
-year = input();
+year = input()
 movie_collecton = {}
-for i in range(1,51):
-	current_coll = tmdb.make_tmdb_api_request(method='/discover/movie', api_key=tmdb.api, extra_params = {'primary_release_year':int(year),'page':i})
-	for j in range(len(current_coll['results'])):
-		movie_collecton.update({current_coll['results'][j]['title']:current_coll['results'][j]})
-		stdout.write("\r%d" % len(movie_collecton)+'|1000')
-		stdout.flush()
-stdout.write("\r  \r\n") # clean up
-collection = open('collection.json','w')
-json.dump(movie_collecton,collection)
+page = 1
+while len(movie_collecton)<1000:
+    current_coll = tmdb.make_tmdb_api_request(method='/discover/movie', api_key=tmdb.api, extra_params={'primary_release_year': int(year), 'page': page})
+    for j in range(len(current_coll['results'])):
+        movie_collecton.update({current_coll['results'][j]['title']: current_coll['results'][j]})
+        stdout.write("\r%d" % len(movie_collecton) + '|1000')
+        stdout.flush()
+        if len(movie_collecton)==1000: break
+    page+=1
+collection = open('collection.json', 'w')
+json.dump(movie_collecton, collection)
 collection.close()
+
 print('Готово!')
